32baca0f960c1c557e8e54e4bb9871ccd35cc32a
Abraham V. R <sneak_89@hotmail.com>
2024-02-26 08:52:48 -0600

    Primer commit

--- Repaso/Program.cs
+++ Repaso/Program.cs
@@ -26,6 +26,6 @@ using Repaso;//Nombre de espacio del proyecto (Libreria)
 //Instancia del objeto 
 Persona persona1 = new Persona("Juan", 19, 276123456);
 
-//Acceso a los metodos del objeto persona 1
+//Acceso aa los metodos del objeto persona 1
 persona1.Caminar();
 persona1.Trabajar(); 
