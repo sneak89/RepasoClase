5ff729fe8de02c08942c27b8c7d2fbb92d835d1a
Abraham V. R <sneak_89@hotmail.com>
2024-02-26 08:52:12 -0600

    Agregar archivos de proyecto.

---
+++ Repaso/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    internal class Persona
+    {
+        //Atributos
+        public string nombre;
+        public int edad;
+        public int telefono; 
+
+        //Constructor
+        public Persona(string nombre, int edad, int telefono)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.telefono = telefono;
+        }
+
+        //Metodos
+        public void Caminar()
+        {
+            Console.WriteLine("La persona esta caminando");
+        }
+
+        public void Trabajar()
+        {
+            Console.WriteLine("La persona esta trabajando"); 
+        }
+    }
+}
---
+++ Repaso/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Repaso de generar clases 
+//Ejemplo de una Persona que trabaja vendiendo flores
+/*Atributos clase persona: 
+ *  Nombre
+ *  Edad
+ *  Telefono
+ * Metodos clase persona:
+ *  Camine
+ *  Trabaje
+ * Atributos clase negocio:
+ *  Nombre
+ *  Direccion
+ *  Telefono
+ * Atributos clase flores:
+ *  Especie
+ *  Color
+ *  Tamaño
+ *  Precio
+ * Método clase flores: 
+ *  Hacer ramo
+ -----------------------------------------------*/
+
+using Repaso;//Nombre de espacio del proyecto (Libreria)
+
+//Instancia del objeto 
+Persona persona1 = new Persona("Juan", 19, 276123456);
+
+//Acceso a los metodos del objeto persona 1
+persona1.Caminar();
+persona1.Trabajar(); 
